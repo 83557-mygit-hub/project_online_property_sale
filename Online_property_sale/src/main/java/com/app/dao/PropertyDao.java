6649b36f5c1330a68f523ad2487283896780b78c
kd2-omkar-83557 <omkardeshmukh080599@gmail.com>
2024-08-13 08:20:58 +0530

    property_typedao

---
+++ Online_property_sale/src/main/java/com/app/dao/PropertyDao.java
@@ -0,0 +1,35 @@
+package com.app.dao;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import com.app.entities.Owner;
+import com.app.entities.Property;
+
+@Transactional
+@Repository
+public interface PropertyDao extends JpaRepository<Property, Integer> {
+	
+	@Modifying
+	@Query("update Property set image=:file where id=:id")
+	public int uploadPhoto(int id,byte[] file);
+	
+	@Query("select p from Property p where p.area_id.id = :id")
+	public List<Property> getbyareaid(int id);
+	
+	@Query("select p from Property p where p.area_id.city_id.id = :id")
+	public List<Property> getbycityid(int id);
+	
+	@Query("SELECT pt.name FROM Property p JOIN p.property_type_id pt WHERE p.id = :propertyId")
+	String findPropertyTypeNameByPropertyId(@Param("propertyId") int propertyId);
+	
+	@Query("select p from Property p where p.owner_id.id = :id")
+	List<Property> findByOwner_id(int id);
+}
